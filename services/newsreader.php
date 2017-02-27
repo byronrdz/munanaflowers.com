aa771363c66b1c8b1d92486715ce3b0128ca8642
Byron Rodriguez <byronrdz@gmail.com>
2017-02-26 20:05:33 -0500

    Codificación de canal RSS a JSON
    Codificación de canal RSS a JSON para presentar listado de noticias en
    el sitio web.

---
+++ services/newsreader.php
@@ -0,0 +1,38 @@
+<?php
+$ind=0;
+if(isset($_GET['ind'])){$ind = intval($_GET['ind']);}
+
+$fuente = 'https://www.munanaflowers.com/pruebas-new-model/munanaflowers.com/services/newschannel.php';
+$noticia = array();
+
+    $xml = simplexml_load_file($fuente);
+
+if($xml){
+	
+ 	$titulo = $xml->channel->item[$ind]->title;
+	$fecha =  $xml->channel->item[$ind]->pubDate;
+    $nota = $xml->channel->item[$ind]->description;
+    $fuente = $xml->channel->item[$ind]->source;	
+    $autor = $xml->channel->item[$ind]->author;
+    $link = $xml->channel->item[$ind]->link;	
+	
+    $noticia['titulo'] =htmlentities((string) $titulo);
+    $noticia['fecha'] =htmlentities((string) $fecha);
+    $noticia['fuente'] =htmlentities((string) $fuente);
+    $noticia['autor'] =htmlentities((string) $autor);	
+    $noticia['link'] =htmlentities((string) $link);	
+
+    $noticia['nota'] =(string) $nota;
+    
+    $noticia = json_encode($noticia);
+	$noticia = str_replace('\\','',$noticia);  
+    header('Content-type: application/json; charset=utf-8');
+    
+    echo($noticia);
+    
+} else {
+	echo 'Error:'.'<br>';
+    exit('Failed to open address.');
+}
+?>
+
